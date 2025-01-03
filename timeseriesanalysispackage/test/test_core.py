1d04db25501585412e2a462a2d7f2339918bff40
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2025-01-03 16:34:35 +0000

    add command line argument tests

--- timeseriesanalysispackage/test/test_core.py
+++ timeseriesanalysispackage/test/test_core.py
@@ -1,6 +1,36 @@
-import pytest
-import timeseriesanalysispackage.core
+import pytest, sys
+from timeseriesanalysispackage.core import main
+from io import StringIO
+from unittest.mock import patch
 
-class TestClass:
-    def test_one(self):
-        assert 1+1 == 2
+
+class Tests:
+
+    # Command Line Argument Tests
+
+    def test_usage_message_with_incorrect_args(self):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout, patch('sys.exit', side_effect=SystemExit) as mock_exit:
+            sys.argv = ['timeseriesanalysispackage', 'run']
+            with pytest.raises(SystemExit): 
+                main()
+
+            output = mock_stdout.getvalue().strip()
+            assert output == "Usage: timeseriesanalysispackage run <user_script.py>"
+            mock_exit.assert_called_once_with(1)
+
+    def test_usage_message_with_invalid_run_argument(self):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout, patch('sys.exit', side_effect=SystemExit) as mock_exit:
+            sys.argv = ['timeseriesanalysispackage', 'invalid_command', 'user_script.py']
+            with pytest.raises(SystemExit): 
+                main()
+
+            output = mock_stdout.getvalue().strip()
+            assert output == "Usage: timeseriesanalysispackage run <user_script.py>"
+            mock_exit.assert_called_once_with(1)
+
+    def test_correct_user_script_argument(self):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            sys.argv = ['timeseriesanalysispackage', 'run', 'user_script.py']
+            main()
+            output = mock_stdout.getvalue().strip()
+            assert output == 'user_script.py'
