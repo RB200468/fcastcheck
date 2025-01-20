bf8ee404b3b91b5aa3845fe3542634c54efec036
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2025-01-20 17:02:24 +0000

    refactor how charts are displayed

--- timeseriesanalysispackage/web/static/scripts/displaychart.js
+++ timeseriesanalysispackage/web/static/scripts/displaychart.js
@@ -1,18 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const ctx = document.getElementById('myChart');
-    
-    var chartsObj = JSON.parse(charts) //String to JSON
+document.addEventListener("activeChartChanged", (event) => {
+    console.log("Active Chart Name: ", event.detail);
+    const chartName = event.detail;
 
+    fetch(`http://localhost:8001/chart?name=${chartName}`)
+        .then(response => {
+            if (!response.ok) {
+                throw Error("Error Occured ", response.statusText)
+            }
+            return response.json();
+        })
+        .then (data => {
+            // Destroy old Canvas
+            const container = document.getElementById('canvas-container');
+            const oldCanvas = document.getElementById('Chart');
+            container.removeChild(oldCanvas);
+
+            // Create New Canvas
+            const newCanvas = document.createElement('canvas');
+            newCanvas.id = 'Chart';
+            newCanvas.ariaLabel = 'Chart';
+            newCanvas.role = 'svg';
+            container.appendChild(newCanvas);
+
+            // Update New Canvas
+            const ctx = newCanvas.getContext('2d');
+            builtChart = buildChart(ctx, data.content);
+        })
+        .catch (error => {
+            console.error("Error: ", error)
+        });
+});
+
+function buildChart(ctx, chartData){
     const timeSeriesData = {
-        labels: chartsObj.labels,
+        labels: chartData.labels,
         datasets: [
             {
-                label: chartsObj.label,
-                data: chartsObj.data,
-                borderColor: chartsObj.borderColor,
-                backgroundColor: chartsObj.borderColor,
-                tension: chartsObj.tension,
-                fill: chartsObj.fill,   
+                label: chartData.label,
+                data: chartData.data,
+                borderColor: chartData.borderColor,
+                backgroundColor: chartData.borderColor,
+                tension: chartData.tension,
+                fill: chartData.fill,   
             }
         ]
     };
@@ -49,7 +78,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     beginAtZero: true,
                     title: {
                         display: true,
-                        text: chartsObj.title,
+                        text: chartData.title,
                         color: txt_color_1
                     },
                     ticks: {
@@ -66,18 +95,59 @@ document.addEventListener('DOMContentLoaded', function() {
         },
     }
 
-    const myChart = new Chart(ctx, config);
-    window.myChart = myChart;
+    const chart = new Chart(ctx, config);
+    return chart;
+}
 
-    window.addEventListener('resize', () => {
-        const isScreenWide = window.innerWidth <= 600;
-        myChart.options.maintainAspectRatio = isScreenWide;
-        myChart.update()
-    })
+window.addEventListener('resize', () => {
+    const isScreenWide = window.innerWidth <= 600;
+    builtChart.options.maintainAspectRatio = isScreenWide;
+    builtChart.update()
+})
+
+/* Dark Colors */
+const chrt_bg_color_1_dark = getComputedStyle(document.documentElement).getPropertyValue('--bg-color-1-dark').trim();
+const chrt_bg_color_2_dark = getComputedStyle(document.documentElement).getPropertyValue('--bg-color-2-dark').trim();
+const chrt_txt_color_1_dark = getComputedStyle(document.documentElement).getPropertyValue('--txt-color-1-dark').trim();
+const chrt_txt_color_2_dark = getComputedStyle(document.documentElement).getPropertyValue('--txt-color-2-dark').trim();
+
+/* Light Colors */
+const chrt_bg_color_1_light = getComputedStyle(document.documentElement).getPropertyValue('--bg-color-1-light').trim();
+const chrt_bg_color_2_light = getComputedStyle(document.documentElement).getPropertyValue('--bg-color-2-light').trim();
+const chrt_txt_color_1_light = getComputedStyle(document.documentElement).getPropertyValue('--txt-color-1-light').trim();
+const chrt_txt_color_2_light = getComputedStyle(document.documentElement).getPropertyValue('--txt-color-2-light').trim();
 
 
+document.addEventListener('themeSwitched', (event) => {
+    if (event.detail == 'dark') {
+        /* Change Chart Dark */
+        builtChart.options.scales.x.ticks.color = chrt_txt_color_1_dark;
+        builtChart.options.scales.y.ticks.color = chrt_txt_color_1_dark;
+        builtChart.options.scales.x.border.color = chrt_txt_color_1_dark;
+        builtChart.options.scales.y.border.color = chrt_txt_color_1_dark;
+        builtChart.options.scales.y.title.color = chrt_txt_color_1_dark;
+        builtChart.options.plugins.legend.labels.color = chrt_txt_color_1_dark;
+
+    } else if (event.detail == 'light') {
+        /* Change Chart Light */
+        builtChart.options.scales.x.ticks.color = chrt_txt_color_1_light;
+        builtChart.options.scales.y.ticks.color = chrt_txt_color_1_light;
+        builtChart.options.scales.x.border.color = chrt_txt_color_1_light;
+        builtChart.options.scales.y.border.color = chrt_txt_color_1_light;
+        builtChart.options.scales.y.title.color = chrt_txt_color_1_light;
+        builtChart.options.plugins.legend.labels.color = chrt_txt_color_1_light;
+    }
+    builtChart.update();
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
     const btn = document.getElementById('updateBtn');
     btn.addEventListener('click', () => {
+        builtChart.options.scales.x.ticks.color = '#19D302';
+        builtChart.update();
         fetch('http://localhost:8001/model?name=MyModel&start=2&steps=2')
             .then(response => {
                 if (!response.ok) {
@@ -115,4 +185,4 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.error('Error: ', error);
             })
     });
-});
+})
