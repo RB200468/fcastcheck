c96e259dcdc1c22b05d18ef6287c79a93c27eab8
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2025-02-09 19:17:38 +0000

    refactor how forecasts work

--- timeseriesanalysispackage/core.py
+++ timeseriesanalysispackage/core.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from inspect import isclass
 from .chart import Chart
-from .forecasting import ForecastingModel
+from .forecasting import ForecastingModel, Forecast
 
 
 app = FastAPI()
@@ -13,6 +13,7 @@ app = FastAPI()
 # Contains JSON Objects
 registered_charts = {}
 registered_models = {}
+registered_forecasts = {}
 
 templatesDir = os.path.join(os.path.dirname(__file__), 'web', 'templates')
 templates = Jinja2Templates(directory=templatesDir)
@@ -28,16 +29,78 @@ def home(request:Request):
     context= {
         'request': request,
         'charts': list(registered_charts.keys()),
-        'models': list(registered_models.keys())
+        'models': list(registered_models.keys()),
+        'forecasts': list(registered_forecasts.keys())
     }
     return templates.TemplateResponse("index.jinja2", context)
 
 @app.get("/chart")
 def get_chart(name: str):
     if name in registered_charts.keys():
-        return JSONResponse(content={'content': registered_charts[name]}, status_code=200)
+        current_forecasts=[]
+        for forecast_name in list(registered_forecasts.keys()):
+            if registered_forecasts[forecast_name].chart == name:
+                current_forecasts.append(forecast_name)
+
+        return JSONResponse(content={'content': registered_charts[name], 'forecasts': current_forecasts}, status_code=200)
     else:
         return JSONResponse(content={'content': "Chart not found"})
+    
+@app.get("/forecast")
+def get_forecast(name: str):
+    if name not in registered_forecasts.keys():
+        return JSONResponse(content={'content': "Forecast not found"})
+    
+    current_forecast = registered_forecasts[name]
+
+    
+    current_chart = registered_charts[current_forecast.chart]
+    # Clear all forecast lines
+    current_chart["datasets"] = [current_chart["datasets"][0]]
+
+    # Get all models
+    current_models = []
+    for model in current_forecast.models:
+        print(model)
+        print(registered_models.keys())
+        if model not in registered_models.keys():
+            return JSONResponse(content={'content': "Model not found"})        
+        current_models.append(registered_models[model])
+
+    # Validate forecast spread and calculate required steps
+    current_chart_labels = current_chart.get('labels')
+    start_date_index = current_chart_labels.index(current_forecast.start_time)
+    end_date_index = current_chart_labels.index(current_forecast.end_time)
+    steps = (end_date_index - start_date_index) + 1
+    if ((start_date_index >= end_date_index) or (start_date_index == 0)):
+        return JSONResponse(content={'content': "Forecast time spread error"})
+
+    # Fit and predict each model
+    training_data = current_chart_labels[0:start_date_index]
+    for i in range(len(current_models)):
+        current_model = current_models[i]()
+        current_model.fit(data=training_data)
+        current_prediction = [None] * len(training_data)
+        current_prediction.extend(current_model.predict(steps=steps))
+        line_color = random_hex_colour()
+
+        current_forecast_line = {
+            "label": current_forecast.models[i],
+            "data": current_prediction,
+            "borderColor": line_color,
+            "backgroundColor": line_color,
+            "borderDash": [5,5]
+        }
+
+        current_chart['datasets'].append(current_forecast_line)
+
+
+    return JSONResponse(content={'content': current_forecast.chart}, status_code=200)
+
+
+
+
+
 
 @app.put("/forecast")
 def put_forecast(chart: str,model: str, start: int, steps: int, name: str):
@@ -105,6 +168,10 @@ def get_user_data(filepath):
             """Checks for ForecastingModel subclasses"""
             registered_models[attr_name] = attr_value
             print(f'Successfully added model: {attr_name}')
+        elif isinstance(attr_value, Forecast):
+            """Checks for Forecast Objects"""
+            registered_forecasts[attr_name] = attr_value
+            print(f"Successfully added forecast: {attr_name}")
 
     if not registered_charts:
         raise ValueError("No Chart objects found in the user's script.")
--- timeseriesanalysispackage/forecasting.py
+++ timeseriesanalysispackage/forecasting.py
@@ -4,3 +4,12 @@ class ForecastingModel:
     
     def predict(self, steps: int) -> list:
         raise NotImplementedError("predict() must be implemented")
+
+
+class Forecast:
+    def __init__(self, chart_name, models, start_time, end_time):
+        self.chart = chart_name
+        self.models = models
+        self.start_time = start_time
+        self.end_time = end_time
+        
