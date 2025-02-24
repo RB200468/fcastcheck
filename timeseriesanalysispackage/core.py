df2f364aecf25bdb93a5c6cdb488e71b0f2f3e96
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2025-02-24 16:06:11 +0000

    refactor forecast pipeline to enable pre computing of models

--- timeseriesanalysispackage/core.py
+++ timeseriesanalysispackage/core.py
@@ -6,6 +6,7 @@ from fastapi.staticfiles import StaticFiles
 from inspect import isclass
 from .chart import Chart
 from .forecasting import ForecastingModel, Forecast
+from .utils import random_hex_colour
 
 
 app = FastAPI()
@@ -14,6 +15,7 @@ app = FastAPI()
 registered_charts = {}
 registered_models = {}
 registered_forecasts = {}
+forecast_lines = {}
 
 templatesDir = os.path.join(os.path.dirname(__file__), 'web', 'templates')
 templates = Jinja2Templates(directory=templatesDir)
@@ -39,7 +41,7 @@ def get_chart(name: str):
     if name in registered_charts.keys():
         current_forecasts=[]
         for forecast_name in list(registered_forecasts.keys()):
-            if registered_forecasts[forecast_name].chart == name:
+            if registered_forecasts[forecast_name].get_chart() == name:
                 current_forecasts.append(forecast_name)
 
         return JSONResponse(content={'content': registered_charts[name], 'forecasts': current_forecasts}, status_code=200)
@@ -53,48 +55,17 @@ def get_forecast(name: str):
     
     current_forecast = registered_forecasts[name]
 
-    
-    current_chart = registered_charts[current_forecast.chart]
+    current_chart_name = current_forecast.get_chart()
+    current_chart = registered_charts[current_chart_name]
     # Clear all forecast lines
     current_chart["datasets"] = [current_chart["datasets"][0]]
 
-    # Get all models
-    current_models = []
-    for model in current_forecast.models:
-        if model not in registered_models.keys():
-            return JSONResponse(content={'content': "Model not found"})        
-        current_models.append(registered_models[model])
+    # Add pre-computed forecast lines
+    forecasts = forecast_lines.get(current_chart_name).get(name)
+    for forecast in forecasts:
+        current_chart['datasets'].append(forecast)
 
-    # Validate forecast spread and calculate required steps
-    current_chart_labels = current_chart.get('labels')
-    start_date_index = current_chart_labels.index(current_forecast.start_time)
-    end_date_index = current_chart_labels.index(current_forecast.end_time)
-    steps = (end_date_index - start_date_index) + 1
-    if ((start_date_index >= end_date_index) or (start_date_index == 0)):
-        return JSONResponse(content={'content': "Forecast time spread error"})
-
-    # Fit and predict each model
-    current_chart_data = current_chart.get('datasets')[0].get('data')
-    training_data = current_chart_data[0:start_date_index]
-    for i in range(len(current_models)):
-        current_model = current_models[i]()
-        current_model.fit(data=training_data)
-        current_prediction = [None] * len(training_data)
-        current_prediction.extend(current_model.predict(steps=steps))
-        line_color = random_hex_colour()
-
-        current_forecast_line = {
-            "label": current_forecast.models[i],
-            "data": current_prediction,
-            "borderColor": line_color,
-            "backgroundColor": line_color,
-            "borderDash": [5,5]
-        }
-
-        current_chart['datasets'].append(current_forecast_line)
-
-
-    return JSONResponse(content={'content': current_forecast.chart}, status_code=200)
+    return JSONResponse(content={'content': current_chart_name}, status_code=200)
 
 
 def get_user_data(filepath):
@@ -132,9 +103,60 @@ def get_user_data(filepath):
         print("No Forecasting Models found in the user's script.'")
 
 
-def random_hex_colour():
-    return "#{:06x}".format(random.randint(0,0xFFFFFF))
+def load_forecasts():
+    for forecast in registered_forecasts.keys():
+        current_forecast = registered_forecasts[forecast]
 
+        if current_forecast.get_chart() not in registered_charts.keys():
+            print(current_forecast.get_chart())
+            return "ERROR: Chart Not Found"
+        current_chart = registered_charts[current_forecast.get_chart()]
+
+        current_models = []
+        for model in current_forecast.get_models():
+            if model not in registered_models.keys():
+                return "ERROR: Model Not Found"
+            current_models.append(registered_models[model])
+
+
+        chart_key = current_forecast.get_chart()
+        if chart_key not in forecast_lines.keys():
+            forecast_lines[chart_key] = {}
+
+        if forecast in forecast_lines.get(chart_key).keys():
+            return "ERROR: Only Unique Forecast Names Allowed"
+
+        forecast_lines[current_forecast.get_chart()][forecast] = []
+
+        # Validate forecast spread and calculate required steps
+        current_chart_labels = current_chart.get('labels')
+        start_date_index = current_chart_labels.index(current_forecast.get_start_time())
+        end_date_index = current_chart_labels.index(current_forecast.get_end_time())
+        steps = (end_date_index - start_date_index) + 1
+        if ((start_date_index >= end_date_index) or (start_date_index == 0)):
+            return "ERROR: Invalid Forecast Time Spread"
+        
+
+        # Fit and predict each model
+        current_chart_data = current_chart.get('datasets')[0].get('data')
+        training_data = current_chart_data[0:start_date_index]
+        for i in range(len(current_models)):
+            current_model = current_models[i]()
+            current_model.fit(data=training_data)
+            current_prediction = [None] * len(training_data)
+            current_prediction.extend(current_model.predict(steps=steps))
+            line_color = random_hex_colour()
+
+            current_forecast_line = {
+                "label": current_forecast.get_models()[i],
+                "data": current_prediction,
+                "borderColor": line_color,
+                "backgroundColor": line_color,
+                "borderDash": [5,5]
+            }
+            forecast_lines[current_forecast.get_chart()][forecast].append(current_forecast_line)
+        
+    return "Forecasts Loaded"
 
 def main():
     if (len(sys.argv) != 3 or sys.argv[1] != "run"):
@@ -145,6 +167,7 @@ def main():
 
     try:
         get_user_data(user_script)
+        print(load_forecasts())
     except Exception as e:
         print(f"Error getting chart: {e}")
         sys.exit(1)
