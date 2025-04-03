e1011ddfa54cebd34aca5e758184e7a0061f6af4
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2025-04-03 13:17:18 +0100

    fix label error and remove redundent code

--- fcastcheck/utils.py
+++ fcastcheck/utils.py
@@ -11,7 +11,7 @@ def random_hex_colour():
     return "#{:06x}".format(random.randint(0,0xFFFFFF))
 
 
-def calc_pred_interval(ground_truth_data, predictions, interval: float = 0.95, window_size: int = 5):
+def calc_pred_interval(ground_truth_data, predictions, interval: float = 0.95):
     ground_truth_data = np.asarray(ground_truth_data)
     predictions = np.asarray(predictions)
     
@@ -38,34 +38,25 @@ def calc_metrics(predictions: list, groundTruth: list) -> dict:
     mean_absolute_error = []
     root_mean_Squared_error = []
     mean_absolute_percentage_error = []
-    symmetric_mape = []
-    mean_absolute_scaled_error = []
 
     for i,_ in enumerate(predictions):
-        mean_absolute_error.append(calc_mae(predictions[0:i+1], groundTruth[0:i+1]))
         root_mean_Squared_error.append(calc_rmse(predictions[0:i+1], groundTruth[0:i+1]))
-        mean_absolute_percentage_error.append(calc_mape(predictions[0:i+1],groundTruth[0:i+1]))
-
+    
     metrics_dict = {
         "MAE": mean_absolute_error,
         "RMSE": root_mean_Squared_error,
-        "MAPE": mean_absolute_percentage_error
+        "MAPE": mean_absolute_percentage_error,
     }
 
     return metrics_dict
 
-
-def calc_mae(predictions: list, groundTruth: list) -> float:
-    # Mean Absolute Error
-    return abs(sum(i-j for i,j in zip(groundTruth,predictions))/len(predictions))
+def calc_residuals(predictions: list, groundTruth: list) -> list:
+    return groundTruth - predictions
 
 def calc_rmse(predictions: list, groundTruth: list) -> float:
     # Root Mean Squared Error
     return math.sqrt(sum(pow(i-j,2) for i,j in zip(groundTruth,predictions))/len(predictions))
 
-def calc_mape(predictions: list, groundTruth: list) -> float:
-    # Mean Absolute Percentage Error
-    return abs(sum((100*(i-j))/i for i,j in zip(groundTruth,predictions))/len(predictions))
 
 
 def make_stationary(data, diff_order=1):
@@ -112,10 +103,10 @@ def cross_validation_bandwidth(data):
 
 
 
-def density_data(rmseValues, x_values):
-    optimal_bandwidth = cross_validation_bandwidth(rmseValues)
+def density_data(values, x_values):
+    optimal_bandwidth = cross_validation_bandwidth(values)
 
-    kde = gaussian_kde(rmseValues, bw_method=optimal_bandwidth / np.std(rmseValues, ddof=1))
+    kde = gaussian_kde(values, bw_method=optimal_bandwidth / np.std(values, ddof=1))
     densityValues = kde(x_values)
 
     return densityValues.tolist()
